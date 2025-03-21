749af80fa8c374c2c2c8941fbd0efb6e08800f50
Saahil Chaudhary <saahilcha1984@gmail.com>
2025-03-22 02:50:01 +0530

    Added Sensors with launch file and core updates.

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,8 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -25,18 +26,33 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds' ,
+        'empty.world'
+    )
+
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world' ,
+        default_value= default_world,
+        description='World to load'
+    )
+
     # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]), 
-                    launch_arguments={'gz_args': 'empty.sdf'}.items()
+                    launch_arguments={'gz_args': [' -r -v4 ' , world], 'on_exit_shutdown': 'true'}.items()
              )
 
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_spawn_model.launch.py')]), 
-                    launch_arguments={'world': 'empty' , 'topic': 'robot_description' , 'entity': 'my_bot'}.items()
+                    launch_arguments={'world': 'empty' , 'topic': 'robot_description' , 'name': 'my_bot'}.items()
     )
 
 
@@ -58,10 +74,18 @@ def generate_launch_description():
         output='screen',
     )
 
+    ros_gz_image_bridge = Node(
+        package= 'ros_gz_image' ,
+        executable= 'image_bridge' ,
+        arguments=['/camera/image_raw']
+    )
+
     # Launch them all!
     return LaunchDescription([
         rsp,
+        world_arg,
         gazebo,
         spawn_entity,
         start_gazebo_ros_bridge_cmd,
+        ros_gz_image_bridge
     ])
